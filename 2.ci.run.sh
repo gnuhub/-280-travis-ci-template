173fb88d2cc2fad4ecd41721156ba50583246a30
stallman <gnuhub@icloud.com>
2020-03-01 20:09:56 +0800

    sync auto commit

--- 2.ci.run.sh
+++ 2.ci.run.sh
@@ -3,7 +3,8 @@
 
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
-
+chmod 600 sf
+ssh-keyscan "shell.sourceforge.net" >> ~/.ssh/known_hosts
 ssh-keyscan "frs.sourceforge.net" >> ~/.ssh/known_hosts
 ssh-keyscan "gitlab.com" >> ~/.ssh/known_hosts
 ssh-keyscan "github.com" >> ~/.ssh/known_hosts
