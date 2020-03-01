1363789c762fb8de4a9bee804bf25a474fbfbf1d
stallman <gnuhub@icloud.com>
2020-03-01 19:21:59 +0800

    add ssh-keyscan auto commit

--- 2.ci.run.sh
+++ 2.ci.run.sh
@@ -4,3 +4,7 @@
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 
+ssh-keyscan "frs.sourceforge.net" >> ~/.ssh/known_hosts
+ssh-keyscan "gitlab.com" >> ~/.ssh/known_hosts
+ssh-keyscan "github.com" >> ~/.ssh/known_hosts
+ssh-keyscan "bitbucket.org" >> ~/.ssh/known_hosts
