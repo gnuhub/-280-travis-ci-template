2df1c436275a46c9fd20d365e0471995e9fa80e4
stallman <gnuhub@icloud.com>
2020-03-02 02:47:52 +0800

    fix sync auto commit

--- 5.packages.elementary.io.sh
+++ 5.packages.elementary.io.sh
@@ -4,8 +4,6 @@
 export CMD_PATH=$(cd `dirname $0`; pwd)
 cd $CMD_PATH
 
-cd ~
+wget --mirror -p --convert-links --no-parent  -P ./ http://packages.elementary.io/
 
-alias wgetmir='wget --mirror -p --convert-links --no-parent  -P'
-
-wgetmir ./ http://packages.elementary.io/
+rsync -e "ssh -i sf" --exclude "sf" --progress -avz ./packages.elementary.io/ gnuhub@frs.sourceforge.net:/home/frs/project/do365/packages.elementary.io/
